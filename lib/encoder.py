d78be7796f7aa40eb8490a1716a77319ce62fe26
Tyler Christiansen <tyler@oss-stack.io>
2014-06-27 11:54:03 -0700

    refactor and remove pyc from git repos

--- lib/encoder.py
+++ lib/encoder.py
@@ -2,12 +2,12 @@ import json
 from jnpr.junos.factory.table import Table
 from jnpr.junos.factory.view import View
 
-class TableJSONEncoder( json.JSONEncoder ):
-  def default(self, obj):
-    if isinstance(obj, View):
-      obj = dict(obj.items())
-    elif isinstance(obj,Table):
-      obj = { item.name: item for item in obj }
-    else:
-      obj = super(TableJSONEncoder, self).default(obj)
-    return obj
+class TableJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, View):
+            obj = dict(obj.items())
+        elif isinstance(obj, Table):
+            obj = {item.name: item for item in obj}
+        else:
+            obj = super(TableJSONEncoder, self).default(obj)
+        return obj
